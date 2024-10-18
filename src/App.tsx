252e760b985a7426e5c7f5ea87647f3b4cb0dc63
Daniel Keane Kelly <danielkeanekelly@gmail.com>
2024-10-18 15:49:05 +0100

    Init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+
+    return (
+        <div className="bg-blue-500">
+
+            <div className="text-red-500">
+                test
+            </div>
+
+        </div>
+    )
+}
+
+export default App
